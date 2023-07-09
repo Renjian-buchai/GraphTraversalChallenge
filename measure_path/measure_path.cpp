5f9c9bbeb9d9df0d5666908d9c6abefa9d2a8da9
ErichToaster <erichroethlisberger0@gmail.com>
2023-07-10 00:46:59 +0200

    Adding validity checks to measure_path

--- measure_path/measure_path.cpp
+++ measure_path/measure_path.cpp
@@ -4,16 +4,32 @@
 
 int measure_path_VEC(std::vector<int> path, std::vector<std::vector<int>> graph) {
 	int toReturn = 0;
+
+	std::vector<bool> wasVisited(graph.size(), false);
+	wasVisited[path[0]] = true;
 	for(int i = 1; i < path.size(); i++) {
+		if(graph[i-1][i] == -1) {return PATH_INVALID_NONEXISTENT_EDGE;}
+		wasVisited[path[i]] = true;
 		toReturn += graph[i-1][i];
+	}
+	for(int i = 0; i < wasVisited.size(); i++) {
+		if(wasVisited[i] == false) {return PATH_INVALID_INCOMPLETE;}
 	}
 	return toReturn;
 }
 
-int measure_path_ARR(std::vector<int> path, int **graph) {
+int measure_path_ARR(std::vector<int> path, int **graph, int graphsize) {
 	int toReturn = 0;
+
+	std::vector<bool> wasVisited(graphsize, false);
+	wasVisited[path[0]] = true;
 	for(int i = 1; i < path.size(); i++) {
+		if(graph[i-1][i] == -1) {return PATH_INVALID_NONEXISTENT_EDGE;}
+		wasVisited[path[i]] = true;
 		toReturn += graph[i-1][i];
+	}
+	for(int i = 0; i < wasVisited.size(); i++) {
+		if(wasVisited[i] == false) {return PATH_INVALID_INCOMPLETE;}
 	}
 	return toReturn;
 }
--- measure_path/measure_path.hpp
+++ measure_path/measure_path.hpp
@@ -3,8 +3,11 @@
 #include <vector>
 #include <string>
 
+#define PATH_INVALID_NONEXISTENT_EDGE	-1 //tries to use an edge which doesn't exist
+#define PATH_INVALID_INCOMPLETE		-2 //doesn't traverse all vertices
+
 
 
 int measure_path_VEC(std::vector<int> path, std::vector<std::vector<int>> graph);
 
-int measure_path_ARR(std::vector<int> path, int **graph);
+int measure_path_ARR(std::vector<int> path, int **graph, int graphsize);
